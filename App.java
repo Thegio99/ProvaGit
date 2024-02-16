04a20fd0bb66e48274a842586163a99e78b7c883
GIOVANNI BATTISTA MERCURIO <gmercuri@emeal.nttdata.com>
2024-02-16 10:51:13 +0100

    Creato calciatore 2

--- App.java
+++ App.java
@@ -4,6 +4,8 @@ public class  App{
 
         Calciatore c1 = new Calciatore("Messi", 10, "AT");
         c1.toString();
+        Calciatore c2 = new Calciatore("Leao", 10, "ATT");
+        c2.toString();
     }
 
 
