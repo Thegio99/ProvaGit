3f566c8444647200d5e525cd5d43b4f24331698f
GIOVANNI BATTISTA MERCURIO <gmercuri@emeal.nttdata.com>
2024-02-21 11:01:50 +0100

    metodo insert

--- App.java
+++ App.java
@@ -1,23 +1,40 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class  App{
+    static Scanner sc =new Scanner(System.in);
+    static ArrayList<Calciatore> playerList= new ArrayList<>();
     public static void main(String[] args){
 
-        Calciatore c1 = new Calciatore("Messi", 10, "AT");
-        Calciatore c2 = new Calciatore("Leao", 10, "ATT");
+        
         
 
-        ArrayList<Calciatore> playerList= new ArrayList<>();
-        playerList.add(c1);
-        playerList.add(c2);
+        System.out.println("Inserire calciatore ?");
+        String s = sc.nextLine();
+        
+        if(s.equalsIgnoreCase("si")){
+            insPlayer();
+        }
 
         for (Calciatore calciatore : playerList) {
             System.out.println(calciatore.toString());
         }
         
-        System.out.println("ERROR");
-
     }
 
+    public static void insPlayer(){
+        System.out.println("Inserisci nome :");
+        String name = sc.nextLine();
+        System.out.println("Inserisci numero Maglia :");
+        int numberShirt = sc.nextInt();
+        sc.nextLine(); // Consuma il carattere di nuova linea nel buffer
+        System.out.println("Inserisci ruolo :");
+        String ruolo = sc.nextLine();
+        
+        Calciatore c = new Calciatore(name, numberShirt, ruolo);
+        playerList.add(c);
+        
+
+    }
 
 }
